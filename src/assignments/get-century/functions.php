d0a3488c04ed794f84ba6c0b74f75d041aef0c96
Karlo Valentic <karlo.valentic@agilo.co>
2022-07-17 22:21:37 +0200

    get_vowels

--- src/assignments/get-century/functions.php
+++ src/assignments/get-century/functions.php
@@ -1,6 +1,8 @@
 <?php
     function get_century($year){
          $century = ceil($year/100);
-            
+         require "/var/www/src/assignments/cardinal-to-ordinal-number/functions.php";
+         $century = cardinal_to_ordinal($century);
+         return 'Year ' . $year . ' belong to the ' . $century .' century.';
     }
 ?>
